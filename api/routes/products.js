fe10563a32513e7c20a1b4e8f6bf12244fb66494
Tuna Ayberk Ozmen <info@tunaayberk.com>
2019-04-07 23:47:35 -0400

    Body Parsing added - CORS Added

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,8 +10,13 @@ router.get('/', (req, res, next) => {
 
 //Handle incoming POST request to /products
 router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    }
     res.status(201).json({
-        message: 'Handling POST requests to /products'
+        message: 'Handling POST requests to /products',
+        createdProduct: product
     });
 });
 
